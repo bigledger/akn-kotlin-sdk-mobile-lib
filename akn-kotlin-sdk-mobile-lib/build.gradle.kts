e238f7823239e89c325c2a1bbe4cff4cab3bf6c7
Shyam Borkar <shyamborkar@wavelet.net>
2023-09-12 05:36:13 +0800

    Added versioning using toml file. Removed unnecessary dependencies in module gradle file.

--- akn-kotlin-sdk-mobile-lib/build.gradle.kts
+++ akn-kotlin-sdk-mobile-lib/build.gradle.kts
@@ -41,37 +41,12 @@ android {
 }
 
 dependencies {
+    implementation(libs.material3)
+    implementation(libs.hilt.navigation.compose)
+    debugImplementation(libs.ui.tooling)
 
-    // NEEDED
-    implementation("androidx.compose.material3:material3:1.1.1")
+    api (libs.akn.client.sdk)
 
-    // Basic
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
-
-    implementation("com.github.bigledger:akn-kotlin-sdk:0.0.4")
-
-//    implementation("androidx.core:core-ktx:1.9.0")
-//    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
-//    implementation("androidx.appcompat:appcompat:1.6.1")
-//    implementation("com.google.android.material:material:1.9.0")
-//    testImplementation("junit:junit:4.13.2")
-//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
 publishing {
@@ -79,7 +54,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.bigledger"
             artifactId = "akn-kotlin-sdk-mobile-lib"
-            version = "0.0.2"
+            version = "0.0.4"
 
             afterEvaluate {
                 from(components["release"])
