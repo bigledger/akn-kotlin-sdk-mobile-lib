33427756b56c4e7e7fa13bac74f8ea0c9b9c5bff
Shyam Borkar <shyamborkar@wavelet.net>
2023-09-19 09:38:52 +0800

    Added common api utils to use in all network related api calls. Updated the akaun client sdk version.

---
+++ akn-kotlin-sdk-mobile-lib/src/main/java/com/akaun/kt/mobile/utils/ApiUtils.kt
@@ -0,0 +1,25 @@
+package com.akaun.kt.mobile.utils
+
+import com.akaun.kt.mobile.core.sharedpreference.CommonPrefHelper
+import com.akaun.kt.mobile.core.sharedpreference.CommonSharedPreferenceConstants
+import com.akaun.kt.sdk.services.comakaunapi.core2.apiservices.shared.Core2Config
+
+data class BasicApiHeader(
+    val authToken: String,
+    val tenantCode: String
+)
+
+fun getHeaders(): BasicApiHeader {
+    val sharedPref = CommonPrefHelper.getPrefs(CommonPrefHelper.LOGIN_PREF_NAME)
+    val authToken = sharedPref.getString(CommonSharedPreferenceConstants.AUTH_TOKEN, "") ?: ""
+    val tenantCode = sharedPref.getString(CommonSharedPreferenceConstants.TENANT_CODE_SELECTED, "") ?: ""
+
+    return BasicApiHeader(authToken = authToken, tenantCode = tenantCode)
+}
+
+fun getBaseUrl(): String {
+    val sharedPref = CommonPrefHelper.getPrefs(CommonPrefHelper.COMMON_PREF_NAME)
+
+    return sharedPref.getString(CommonSharedPreferenceConstants.BASE_URL, "")
+        ?: Core2Config.CLOUD_URL
+}
