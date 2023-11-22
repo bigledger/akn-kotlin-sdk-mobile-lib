c213387797cbecca673350fb18d370efe6636d55
Tan Jun Yu <junyu@wavelet.net>
2023-11-22 14:19:35 +0800

    debugging authTokenExpiry

--- akn-kotlin-sdk-mobile-lib/src/main/java/com/akaun/kt/mobile/screens/splash/SplashViewModel.kt
+++ akn-kotlin-sdk-mobile-lib/src/main/java/com/akaun/kt/mobile/screens/splash/SplashViewModel.kt
@@ -1,6 +1,7 @@
 package com.akaun.kt.mobile.screens.splash
 
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import com.akaun.kt.mobile.core.sharedpreference.CommonPrefHelper
@@ -25,14 +26,19 @@ class SplashViewModel: ViewModel() {
         val sharedPreferences = CommonPrefHelper.getPrefs(CommonPrefHelper.LOGIN_PREF_NAME)
         val authToken =  sharedPreferences.getString(CommonSharedPreferenceConstants.AUTH_TOKEN,null)
 
+        Log.d("authToken", "isAuthTokenExpired: ${authToken}")
         val authTokenMap = authToken?.let { decodeAuthToken(it) }
 
         val expTime = authTokenMap?.get("exp")
+
+        Log.d("expTime", "isAuthTokenExpired: ${expTime.toString()} ")
 
         if(expTime != null){
             val localDateTimeExpiry = convertUnixTimestampToLocalDateTime(expTime.toString())
             // TODO : CHANGE IT BACK
 //            return LocalDateTime.now() >= localDateTimeExpiry
+            Log.d("time", "isAuthTokenExpired: ${LocalDateTime.now().plusDays(35)}")
+            Log.d("localDateTImeExpiy", "isAuthTokenExpired: ${localDateTimeExpiry} ")
             return LocalDateTime.now().plusDays(35) >= localDateTimeExpiry
         }
 
